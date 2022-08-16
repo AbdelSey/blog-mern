d774239426a3ed8a23408b411268236cb17d74c4
abdelsey <abdel.sey01@gmail.com>
2022-08-16 13:12:12 -0700

    mern stack

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "375px",
+      // => @media (min-width: 375px) { ... }
+
+      md: "500px",
+      // => @media (min-width: 500px) { ... }
+
+      lg: "765px",
+      // => @media (min-width: 765px) { ... }
+
+      xl: "900px",
+      // => @media (min-width: 900px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      backgroundImage: {},
+    },
+  },
+
+  plugins: [],
+};
